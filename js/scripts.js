28d60cbcac2a55ce398ee3994f2b3491220d0dff
jasonderekkhan@gmail.com <jasonderekkhan@gmail.com>
2020-06-09 13:40:07 -0700

    Add event handlers and send value to variable
    Co-authored-by:  Joseph Pearce <joseph.pearce@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
   $("form#transportation_survey").submit(function() {
     event.preventDefault();
-    $("").show();
-
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function() {
+      const workTransportationMode = $(this).val();
+      $('#work-responses').append(workTransportationMode + "<br>");
+    });
+    $('#transportation_survey').hide();
   });
 });
